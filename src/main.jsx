4dda8737b84f73173bf3eaa8d1daecb4e4cae5c5
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-11-10 16:38:37 +1030

    Setup react/vite development environment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './css/normalize.css'
+import './css/global.css'
+import App from './components/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
