dea86748091de1c1c66cb73709022d250f7c7f8e
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-11-25 04:47:23 +1030

    Render full project data in Projects component
    - Show all project data from imported data module in Projects
    
    - Replaced project data placeholders with real data
    
    - Minor CSS tweaks

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,11 +18,37 @@ export default function Projects() {
             <div className="projects-grid">
                 {projects.map((p) => {
                     return(
+                        // Iterare through each project
                         <div key={p.title} className="project-item">
+                            {/* Render project thumbnail */}
                             <img src={p.thumbnail} alt={`Thumbnail for ${p.title}`}/>
+
+                            {/* Render project title and description */}
                             <div className="info">
                                 <h3>{p.title}</h3>
                                 <p>{p.description}</p>
+                            </div>
+
+                            {/* Render the disclaimer if it exists */}
+                            {p.disclaimer && 
+                                <div className={`disclaimer ${darkMode ? "dark" : ""}` }>
+                                    <p><i>{p.disclaimer}</i></p>
+                                </div>
+                            }
+
+                            {/* Render the buttons */}
+                            <div className="buttons-container">
+                                {p.github && (
+                                    <a href={p.github} target="_blank" rel="noopener noreferrer" className="github">
+                                        GitHub
+                                    </a>
+                                )}
+
+                                {p.liveDemo && (
+                                    <a href={p.liveDemo} target="_blank" rel="noopener noreferrer" className="demo">
+                                        Live Demo
+                                    </a>
+                                )}
                             </div>
                         </div>
                     )
--- src/data.js
+++ src/data.js
@@ -1,29 +1,33 @@
-import kieransSoundsImg from "./assets/images/thumbnails/thumbnail_kierans_sounds.JPG";
+import kieransSoundsImg from "./assets/images/thumbnails/thumbnail_kierans_sounds.jpg";
+import waldoImg from "./assets/images/thumbnails/thumbnail_waldo.jpg";
+import comingSoonImg from "./assets/images/thumbnails/thumbnail_coming_soon.jpg";
+
+const disclaimer = "Note: Hosted on Render's free trial. Initial load may take 30-60 seconds as the servers wake up.";
 
 export const projects = [
     {
         title: "Kieran's Sounds",
-        description: "Description goes here",
+        description: `A full-stack relaxation noise application that allows users to create accounts,
+         upload custom sounds and songs, and manage their own playlists.`,
         thumbnail: kieransSoundsImg,
         github: "https://github.com/Kieran-Go/kierans-sounds",
         liveDemo: "https://kierans-sounds.vercel.app/",
-        showRenderDisclaimer: true,
+        disclaimer: disclaimer,
     },
     {
-        title: "Kieran's Sounds",
-        description: "Description goes here",
-        thumbnail: kieransSoundsImg,
-        github: "https://github.com/Kieran-Go/kierans-sounds",
-        liveDemo: "https://kierans-sounds.vercel.app/",
-        showRenderDisclaimer: true,
+        title: "Where's Waldo?",
+        description: `A full-stack "Where's Waldo?" web app where players search interactive scenes, submit their
+         completion times and compete on a live leaderboard.`,
+        thumbnail: waldoImg,
+        github: "https://github.com/Kieran-Go/wheres-waldo",
+        liveDemo: "https://wheres-waldo-swart.vercel.app/",
+        disclaimer: disclaimer,
     },
     {
-        title: "Kieran's Sounds",
-        description: "Description goes here",
-        thumbnail: kieransSoundsImg,
-        github: "https://github.com/Kieran-Go/kierans-sounds",
-        liveDemo: "https://kierans-sounds.vercel.app/",
-        showRenderDisclaimer: true,
+        title: "More Projects Coming Soon",
+        description: `More full-stack projects are on the way. I’m actively building and refining new apps to 
+         showcase my work and continue growing as a developer.`,
+        thumbnail: comingSoonImg,
     },
 ]
 
