5dd4a017c26e6f59ab88a06974c30685677ee8b4
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-11-28 09:30:18 +1030

    Refactor CSS with variables and improve styling
    - Created vars.css for reused CSS values and updated all CSS components
     to use variables from vars.css
    
    - Made minor styling adjustments for improved aesthetics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,22 @@ import Footer from "./Footer"
 import Header from "./Header"
 import Home from "./Home"
 import Projects from "./Projects"
+import { useContext } from "react"
+import { DarkModeContext } from "../contexts/darkModeContext"
 
 export default function App() {
+  // ----- CONTEXTS -----
+  const { darkMode } = useContext(DarkModeContext);
+
   // ----- RENDER -----
   return (
-    <>
+    <div className={`content ${darkMode ? "dark" : ""}`}>
       <Header />
       <Home />
       <About />
       <Projects />
       <Contact />
       <Footer />
-    </>
+    </div>
   )
 }
